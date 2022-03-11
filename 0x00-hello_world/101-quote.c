42098c120d0b2e0786d67d9d01bb155e618f7009
mb3ch3 <rhowdan@gmail.com>
2022-03-10 23:30:11 -0800

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * Return: 1
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-		char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-		long l = 59;
-		long fd = 1;
-		long syscall = 1;
-		long ret = 0; __asm__ ("syscall" : "=a" (ret) : "a" (syscall),
-		"D" (fd),
-		"S" (s),
-		"d" (l));
+
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 		return (1);
 
-}							
+}
