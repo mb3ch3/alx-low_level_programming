3063508594b0317c04e3495a7288d1b176ef04c9
mb3ch3 <rhowdan@gmail.com>
2022-03-10 22:22:27 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
-* main - Description
+* main - Entry point
 *
-* Return: Always 0 (Success)
-/*
+* description: prints ""Programming is like building a multilingual puzzle\n" 
+*
+* return: 0
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }
