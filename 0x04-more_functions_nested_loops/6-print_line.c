80334eff9f6ec826f602770e646ed4ca9d755679
mb3ch3 <rhowdan@gmail.com>
2022-03-17 14:28:10 -0700

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
+ * print_line - draw a straight line
+ * @n: number of times underscore is printed
+ * Description: Can only use _putchar to print
  */
 
 void print_line(int n)
 {
-	if (n <= 0)
+	int c;
+
+	c = 0;
+
+	while (c < n)
 	{
-		_putchar('\n');
-	} else
-	{
-		int i;
-		
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar('_');
+		c++;
 	}
+	_putchar('\n');
 }
