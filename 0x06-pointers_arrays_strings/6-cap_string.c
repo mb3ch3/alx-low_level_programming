da2783f9452e6db83f261e63ed8d7093b9aec006
mb3ch3 <rhowdan@gmail.com>
2022-03-23 14:51:16 -0700

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,56 +1,37 @@
-#include "main.h
+#include "main.h"
 
 /**
- * separators - checks and ensure that all string is capitalized
- * @c: character to be checked
- * Return: if separator return 1. Otherwise return 0;
- */
-
-int separator(char c)
-{
-	switch (c)
-	{
-		case ' ':
-		case '\t':
-		case '\n':
-		case ',':
-		case ';':
-		case '.':
-		case '!':
-		case '?':
-		case '"':
-		case '(':
-		case ')':
-		case '{':
-		case '}':
-			return (1);
-		default:
-			return (0);
-	}
-}
-
-/**
- * cap_string - capitalizes all words of a string
- * @s: string to uppercase
- * Return: returns the modified string
+ * cap_string - capitalizes every first letter of a word in a string.
+ * separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ * @s: pointer to string.
+ * Return: pointer to s.
  */
 
 char *cap_string(char *s)
 {
-	int count, upper;
-
-	upper = -32;
+	int count;
+	/*  scan through string */
 
 	count = 0;
 
 	while (s[count] != '\0')
-	{
-		if (s[count] >= 'a' && s[count] <= 'z')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-			if (s[count] == *s || separator(s[count - 1]))
-				s[count] += upper;
+			s[0] = s[0] - 32;
 		}
-
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+				|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+				|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+				|| s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
 		count++;
 	}
 
