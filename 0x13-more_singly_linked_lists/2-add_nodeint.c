5c2abb04f331472a83622b0bd17eb83d25fbdb6d
mb3ch3 <33735505+mb3ch3@users.noreply.github.com>
2022-04-25 19:52:04 +0300

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,19 +4,20 @@
 /**
  * listint_len - Returns the number of elements
  *               in a linked listint_t list.
- * @h: A pointer to the head of the listint_t list.
- *
+ * @head: A pointer to the head of the listint_t list.
  * Return: The number of elements in the listint_t list.
  */
-listint_len(const listint_t *h)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	size_t nodes = 0;
+	listint_t *new;
 
-	while (h)
-	{
-		nodes++;
-		h = h->next;
-	}
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 
-	return (nodes);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
