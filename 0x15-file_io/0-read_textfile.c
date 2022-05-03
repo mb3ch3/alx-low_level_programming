c54bb97e2f0e3213641dc28cb2753dcdd1fa5345
mb3ch3 <33735505+mb3ch3@users.noreply.github.com>
2022-05-03 23:07:07 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,8 @@
  * @letters: number of letters to read and print
  * Return: the number of letters printed, or 0 if it failed
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+
+size_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	int s, t;
