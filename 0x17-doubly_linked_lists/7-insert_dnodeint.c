2f1317000ae7751262ee372e5135744d3b01d460
mb3ch3 <33735505+mb3ch3@users.noreply.github.com>
2022-06-09 23:47:00 +0300

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,43 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at specific index
- * @h: head of linked list
- * @idx: index of new node
- * @n: new node value
- * Return: inserted node
+ * insert_dnodeint_at_index - Insert a new node in the given index
+ * @h: Memory address of the head list
+ * @idx: Index of the list
+ * @n: New data (int)
+ *
+ * Return: the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current;
-	dlistint_t *new;
+	dlistint_t *new = NULL;
+	dlistint_t *head = *h, *prev = *h;
+	unsigned int i;
 
-	if (h == NULL)
-		return (0);
+	if (h == NULL || (head == NULL && idx > 0))
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	current = *h;
-
-	while (idx != 0)
+	head = head->next;
+	for (i = 1; i < idx; i++)
 	{
-		current = current->next;
-		idx--;
-		if (current == NULL)
+		if (prev->next == NULL)
 			return (NULL);
+		head = head->next;
+		prev = prev->next;
 	}
+	if (head == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
-
 	if (new == NULL)
-	{
-		free(new);
 		return (NULL);
-	}
-
 	new->n = n;
-	new->next = current;
-	new->prev = current->prev;
-	if (current->prev != NULL)
-		current->prev->next = new;
-
-	/*TODO: Handle special case when idx is 0 and last index*/
-
-	return (current);
+	new->next = head;
+	new->prev = prev;
+	prev->next = new;
+	if (head != NULL)
+		head->prev = new;
+	return (new);
 }
-
-
