7fd282e06aa8e34216f39ad796ab297586b4abf2
mb3ch3 <33735505+mb3ch3@users.noreply.github.com>
2022-06-09 23:25:38 +0300

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,37 +1,34 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_dnodeint_end - adds node to end of doubly linked list
- * @head: pointer to head of list
- * @n: integer value of node
- * Return: pointer to node added
+ * add_dnodeint_end - add new node at the start
+ * @head: head pinter
+ * @n: the value to add
+ *
+ * Return: pointer
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *newNode;
-	dlistint_t *current = *head;
+	dlistint_t *new_node, *tmp_node;
 
-	newNode = malloc(sizeof(dlistint_t));
-	if (!newNode)
-	{
-		dprintf(2, "Error: Can't malloc\n");
+	if (head == NULL)
 		return (NULL);
-	}
-	newNode->n = n;
-	newNode->next = NULL;
-
-	if (!current)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
 	{
-		newNode->prev = NULL;
-		*head = newNode;
-		return (newNode);
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
 	}
-
-	while (current->next)
-	{
-		current = current->next;
-	}
-	current->next = newNode;
-	newNode->prev = current;
-	return (newNode);
+	tmp_node = *head;
+	while (tmp_node->next != NULL)
+		tmp_node = tmp_node->next;
+	tmp_node->next = new_node;
+	new_node->prev = tmp_node;
+	return (new_node);
 }
