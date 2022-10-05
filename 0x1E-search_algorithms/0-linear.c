be4a2d2c1819112ae65c02a9bf096a8d23ca5c66
mb3ch3 <33735505+mb3ch3@users.noreply.github.com>
2022-10-05 09:46:54 +0300

    Add files via upload

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+* linear_search - linear search algorithm
+* @array: int array to look into
+* @size: size_t size of the array
+* @value: int value to search for in the array
+* Return: index of first match or -1 if any
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
